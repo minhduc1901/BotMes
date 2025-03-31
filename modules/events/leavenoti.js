bd9d3a5f4992124573c67860c43d23aff2ec41d4
minhduc1901 <mduc19104@gmail.com>
2025-04-01 05:07:10 +0700

    Update leavenoti.js

--- modules/events/leavenoti.js
+++ modules/events/leavenoti.js
@@ -1,77 +1,60 @@
 module.exports.config = {
-    name: "leavenoti",
-    eventType: ["log:unsubscribe"],
-    version: "1.0.0",
-    credits: "HĐGN",
-    description: "Thông báo Bot hoặc người dùng rời khỏi nhóm + shareContact",
-    dependencies: {
-        "fs-extra": "",
-        "path": ""
-    }
+  name: "leaveNoti",
+  eventType: ["log:unsubscribe"],
+  version: "1.0.0",
+  credits: "HĐGN",//mod lại by Tòn
+  description: "Thông báo bot hoặc người rời khỏi nhóm có random gif/ảnh/video",
+  dependencies: {
+    "fs-extra": "",
+    "path": ""
+  }
 };
-
-const checkttPath = __dirname + '/../commands/_checktt/'
-
 
 module.exports.onLoad = function () {
     const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
     const { join } = global.nodemodule["path"];
 
-    const path = join(__dirname, "cache", "leaveGif");
-    if (existsSync(path)) mkdirSync(path, { recursive: true });
+  const path = join(__dirname, "cache", "leaveGif");
+  if (existsSync(path)) mkdirSync(path, { recursive: true });	
 
-    const path2 = join(__dirname, "cache", "leaveGif");
+  const path2 = join(__dirname, "cache", "leaveGif", "randomgif");
     if (!existsSync(path2)) mkdirSync(path2, { recursive: true });
 
     return;
 }
 
-module.exports.run = async function ({ api, event, Users, Threads }) {
-    if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
-    const { createReadStream, existsSync, mkdirSync, readdirSync, readFileSync, writeFileSync } = global.nodemodule["fs-extra"];
-    const { join } = global.nodemodule["path"];    
-    const { threadID } = event;
-    const moment = require("moment-timezone");
-    const time = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss - DD/MM/YYYY");
-    const hours = moment.tz("Asia/Ho_Chi_Minh").format("HH");
-    var thu = moment.tz('Asia/Ho_Chi_Minh').format('dddd');
-  if (thu == 'Sunday') thu = 'Chủ Nhật'
-  if (thu == 'Monday') thu = 'Thứ Hai'
-  if (thu == 'Tuesday') thu = 'Thứ Ba'
-  if (thu == 'Wednesday') thu = 'Thứ Tư'
-  if (thu == "Thursday") thu = 'Thứ Năm'
-  if (thu == 'Friday') thu = 'Thứ Sáu'
-  if (thu == 'Saturday') thu = 'Thứ Bảy'
-    const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
-    const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-    const uid =  (event.logMessageData.leftParticipantFbId);
-    const type = (event.author == event.logMessageData.leftParticipantFbId) ? "Đã tự động rời khỏi nhóm." : "Đã bị Quản trị viên xóa khỏi nhóm.";
-    const path = join(__dirname, "cache", "leaveGif");
-    const gifPath = join(path, `bye.gif`);
-    var msg, formPush
+module.exports.run = async function({ api, event, Users, Threads }) {
+  if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
+  const { createReadStream, existsSync, mkdirSync, readdirSync } = global.nodemodule["fs-extra"];
+  const { join } =  global.nodemodule["path"];
+  const { threadID } = event;
+  const moment = require("moment-timezone");
+  const hours = moment.tz("Asia/Ho_Chi_Minh").format("HH");
+  const time = moment.tz("Asia/Ho_Chi_Minh").format("DD/MM/YYYY || HH:mm:s");
+  const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
+  const iduser = event.logMessageData.leftParticipantFbId;
+  const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+  const type = (event.author == event.logMessageData.leftParticipantFbId) ? "𝒗𝒊̀ 𝒌𝒉𝒐̂𝒏𝒈 𝒕𝒉𝒆̂̉ 𝒄𝒉𝒖𝒏𝒈 𝒔𝒐̂́𝒏𝒈 𝒗𝒐̛́𝒊 𝒎𝒐̣𝒊 𝒏𝒈𝒖̛𝒐̛̀𝒊 𝒕𝒓𝒐𝒏𝒈 𝒏𝒉𝒐́𝒎 𝒏𝒆̂𝒏 đ𝒂̃ 𝒕𝒖̛̣ 𝒈𝒊𝒂́𝒄 𝒓𝒂 𝒅𝒊 𝒎𝒂̃𝒊 𝒎𝒂̃𝒊" : "𝒗𝒊̀ 𝒌𝒉𝒐̂𝒏𝒈 đ𝒖̉ đ𝒂̆̉𝒏𝒈 𝒄𝒂̂́𝒑 đ𝒆̂̉ 𝒄𝒐́ 𝒕𝒉𝒆̂̉ 𝒐̛̉ đ𝒂̂𝒚 𝒍𝒂̂𝒖 𝒅𝒂̀𝒊 𝒏𝒆̂𝒏 đ𝒂̃ 𝒃𝒊̣ 𝒒𝒖𝒂̉𝒏 𝒕𝒓𝒊̣ 𝒗𝒊𝒆̂𝒏 𝒄𝒉𝒐 𝒓𝒂 đ𝒂̉𝒐 𝒗𝒊̃𝒏𝒉 𝒗𝒊𝒆̂̃𝒏";
+  const path = join(__dirname, "cache", "leaveGif");
+  const gifPath = join(path, `${threadID}.mp4`);
+  var msg, formPush
 
-    if (existsSync(checkttPath + threadID + '.json')) {
-        const threadData = JSON.parse(readFileSync(checkttPath + threadID + '.json'));
-        const userData_week_index = threadData.week.findIndex(e => e.id == event.logMessageData.leftParticipantFbId);
-        const userData_day_index = threadData.day.findIndex(e => e.id == event.logMessageData.leftParticipantFbId);
-        const userData_total_index = threadData.total.findIndex(e => e.id == event.logMessageData.leftParticipantFbId);
-        if (userData_total_index != -1) {
-            threadData.total.splice(userData_total_index, 1);
-        }
-        if (userData_week_index != -1) {
-            threadData.week.splice(userData_week_index, 1);
-        }
-        if (userData_day_index != -1) {
-            threadData.day.splice(userData_day_index, 1);
-        }
+  if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-        writeFileSync(checkttPath + threadID + '.json', JSON.stringify(threadData, null, 4));
+  (typeof data.customLeave == "undefined") ? msg ="•[⚜️] 𝒕𝒉𝒂̀𝒏𝒉 𝒗𝒊𝒆̂𝒏 𝒎𝒂𝒏𝒈 𝒕𝒆̂𝒏 {name} {type} [⚜️]\n●▬▬▬▬๑۩۩๑▬▬▬▬●\n\n🌺🌸🌺𝒅𝒖̀ 𝒔𝒂𝒐 𝒕𝒉𝒊̀ 𝒄𝒖̃𝒏𝒈 𝒄𝒂̉𝒎 𝒐̛𝒏 𝒃𝒂̣𝒏 {name} đ𝒂̃ đ𝒐̂̀𝒏𝒈 𝒉𝒂̀𝒏𝒉 𝒄𝒖̀𝒏𝒈 𝒄𝒉𝒖́𝒏𝒈 𝒕𝒐̂𝒊 𝒕𝒓𝒐𝒏𝒈 𝒕𝒉𝒐̛̀𝒊 𝒈𝒊𝒂𝒏 𝒒𝒖𝒂 😊💔\n 🌸𝗣𝗥𝗢𝗙𝗜𝗟𝗘: https://facebook.com/profile.php?id={uid}\n\n[] 𝒕𝒉𝒐̛̀𝒊 𝒈𝒊𝒂𝒏 𝒐𝒖𝒕 𝒏𝒉𝒐́𝒎 : 𝒃𝒖𝒐̂̉𝒊 {session} || ⏰" + time : msg = data.customLeave;
+  msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type)
+  .replace(/\{iduser}/g, iduser).replace(/\{session}/g, hours <= 10 ? "𝒔𝒂́𝒏𝒈" : 
+    hours > 10 && hours <= 12 ? "𝒕𝒓𝒖̛𝒂" :
+    hours > 12 && hours <= 18 ? "𝒄𝒉𝒊𝒆̂̀𝒖" : "𝒕𝒐̂́𝒊");
+
+  const randomPath = readdirSync(join(__dirname, "cache", "leaveGif", "randomgif"));
+
+  if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
+  else if (randomPath.length != 0) {
+    const pathRandom = join(__dirname, "cache", "leaveGif", "randomgif", `${randomPath[Math.floor(Math.random() * randomPath.length)]}`);
+    formPush = { body: msg, attachment: createReadStream(pathRandom) }
+  }
+  else formPush = { body: msg }
+
+  return api.sendMessage(formPush, threadID);
     }
-    if (existsSync(path)) mkdirSync(path, { recursive: true });
-
-    (typeof data.customLeave == "undefined") ? msg = "[ Thành Viên Thoát Nhóm ]\n─────────────────\n👤 Thành viên: {name}\n📌 Lý do: {type}\n📆 Thoát nhóm vào lúc {thu}\n⏰ Thời gian: {time}" : msg = data.customLeave;
-    msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type).replace(/\{time}/g, time).replace(/\{uid}/g, uid).replace(/\{thu}/g, thu); 
-    return api.sendMessage(threadID, async () => {
-await api.shareContact(`${msg}`, event.logMessageData.leftParticipantFbId, threadID);
-});
-}
